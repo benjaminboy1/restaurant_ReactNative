ab962850c7b9eb57d2c53f1d8f3c5b86e8911488
Benjamin Mumbita <benjaminmumbita10@gmail.com>
2023-07-23 16:11:47 +0100

    ended

---
+++ src/component/benja.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const benja = () => {
+  return (
+    <View>
+      <Text>benja</Text>
+    </View>
+  )
+}
+
+export default benja
+
+const styles = StyleSheet.create({})
